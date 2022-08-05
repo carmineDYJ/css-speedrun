482ee4d4c95548101e24c7ca134446b66c859280
carmineding tx m1 mbp <carmineding@tencent.com>
2022-08-05 18:16:32 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSS竞速
+本项目灵感来源 [CSS Speedrun](https://github.com/Vincenius/css-speedrun)
---
+++ src/components/Nav.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <img class="css3-svg" src="../assets/icons/css3.svg" />
+    <h1>CSS 速通</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  padding-left: 40vw;
+  padding-right: 40vw;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  background: #121111;
+  >.css3-svg {
+    height: 40px;
+  }
+  >h1 {
+    color: #ffffff;
+  }
+}
+</style>
