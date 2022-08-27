3f4bd5dc23e180c62449006e881154b93d643cc0
carmine_dyj <dyujie@outlook.com>
2022-08-27 21:39:59 +0800

    use details tag to complete usageIntroduction component

--- src/components/UsageIntroduction.vue
+++ src/components/UsageIntroduction.vue
@@ -4,9 +4,17 @@
 
 <template>
   <div class="usage-introduction-wrapper">
-    <div class="usage-introduction">
-      我是怎么工作的
-    </div>
+    <details class="usage-introduction">
+      <summary>我如何运作</summary>
+      <div class="usage-introduction-content">
+        <div>
+          <p>输入CSS选择器，使得仅有被箭头&ensp;</p>
+          <img class="hint-arrow-svg" src="../assets/icons/hint_arrow.svg" />
+          <p>&ensp;指向的DOM元素被选中，然后点击提交按钮</p>
+        </div>
+        <p>在你成功完成热身题后，计时器将开始计时</p>
+      </div>
+    </details>
   </div>
 </template>
 
@@ -14,6 +22,7 @@
 .usage-introduction-wrapper {
   display: flex;
   justify-content: center;
+  align-items: center;
   color: white;
   padding: 0 12px;
 
@@ -25,6 +34,19 @@
     border-radius: 4px;
     padding: 12px 6px;
     background: #111;
+
+    .usage-introduction-content {
+      padding: 0 16px;
+      div:first-child {
+        display: flex;
+        align-items: center;
+
+        .hint-arrow-svg {
+          height: 1em;
+        }
+      }
+    }
+
   }
 }
 </style>
