3e0ac96452863d4939ce624c740d6749e65e5d2c
carmineding tx m1 mbp <carmineding@tencent.com>
2022-09-06 18:06:48 +0800

    adjust TopNav layout, make room for button which could switch dark and light mode, try add class to <html> in TopNav component

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,14 +1,20 @@
 <script setup>
-
+const rootHTMLElement = document.querySelector('html')
+rootHTMLElement.classList.add("fuck");
 </script>
 
 <template>
-  <div class="top-nav-wrapper">
-    <div class="top-nav">
-      <img class="css3-svg" src="../assets/icons/css3.svg" />
-      <h1>CSS速通挑战</h1>
+  <nav class="top-nav-wrapper">
+    <div class="top-nav-inner-wrapper">
+      <div class="top-nav-title">
+        <img class="css3-svg" src="../assets/icons/css3.svg" />
+        <h1>CSS速通挑战</h1>
+      </div>
+      <div class="switch-dark-light-mode-button">
+        a
+      </div>
     </div>
-  </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
@@ -17,34 +23,44 @@
   background: #111;
   justify-content: center;
   padding: 6px 0;
-  >.top-nav {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    >.css3-svg {
-      height: 60px;
-      margin-right: 24px;
-    }
 
-    >h1 {
-      color: #ffffff;
-      font-family: 'ZCOOLQingKeHuangYou-Regular';
-      font-size: 48px;
+  >.top-nav-inner-wrapper {
+    width: 688px;
+    display: flex;
+    justify-content: space-between;
+
+    >.top-nav-title {
       display: flex;
-      align-items: center;
+
+      >.css3-svg {
+        height: 60px;
+        margin-right: 24px;
+      }
+
+      >h1 {
+        color: #ffffff;
+        font-family: 'ZCOOLQingKeHuangYou-Regular';
+        font-size: 48px;
+        display: flex;
+        align-items: center;
+      }
     }
   }
 
 }
+
 @media (max-width: 700px) {
   .top-nav-wrapper {
-    > .top-nav {
-      > .css3-svg {
-        height: 40px;
-        margin-right: 16px;
-      }
-      >h1 {
-        font-size: 32px;
+    >.top-nav-inner-wrapper {
+      >.top-nav-title {
+        >.css3-svg {
+          height: 40px;
+          margin-right: 16px;
+        }
+
+        >h1 {
+          font-size: 32px;
+        }
       }
     }
   }
